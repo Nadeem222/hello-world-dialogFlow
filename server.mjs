59fb22f7db58b68e4e614a5788d51d90d6e6d8b0
Nadeem Hakeem <110339895+Nadeem222@users.noreply.github.com>
2023-03-15 23:43:16 +0500

    some work

--- server.mjs
+++ server.mjs
@@ -4,38 +4,70 @@ import moment from 'moment';
 import momentTZ from 'moment-timezone';
 import morgan from 'morgan';
 
-
-
-
-
-
-
-
-
 const app = express()
 app.use(express.json())
 app.use(morgan('dev'))
 
 
-const port = process.env.PORT || 5001;
+const PORT = process.env.PORT || 3001;
+
+const webhook = {
+  "responseId": "response-id",
+  "session": "projects/project-id/agent/sessions/session-id",
+  "queryResult": {
+    "queryText": "End-user expression",
+    "parameters": {
+      "param-name": "param-value"
+    },
+    "allRequiredParamsPresent": true,
+    "fulfillmentText": "Response configured for matched intent",
+    "fulfillmentMessages": [
+      {
+        "text": {
+          "text": [
+            "Response configured for matched intent"
+          ]
+        }
+      }
+    ],
+    "outputContexts": [
+      {
+        "name": "projects/project-id/agent/sessions/session-id/contexts/context-name",
+        "lifespanCount": 5,
+        "parameters": {
+          "param-name": "param-value"
+        }
+      }
+    ],
+    "intent": {
+      "name": "projects/project-id/agent/intents/intent-id",
+      "displayName": "matched-intent-name"
+    },
+    "intentDetectionConfidence": 1,
+    "diagnosticInfo": {},
+    "languageCode": "en"
+  },
+  "originalDetectIntentRequest": {}
+}
+
+app.get("/ping" , (req , res ) => {
+  res.send("ping back");
+})
 
 
 
-app.post('/webhook', async (req, res) => {
-
-  
-  
+app.post("/webhook", async (req, res) => {
   try {
   
     const body = req.body;
 
-    const intentName = body.queryResult.intent.displayName
-    const params = body.queryResult.parameters
+    const intentName = body.queryResult.intent.displayName;
+    const params = body.queryResult.parameters;
 
     if(intentName === "Default Welcome Intent"){
       const currentTime = momentTZ.tz(moment(), "Asia/Karachi");
 
-      const currentHour = +moment(currentTime).format('HH');
+      const currentHour = moment(currentTime).format('HH');
       console.log(currentHour);
 
       let greeting = "";
@@ -71,18 +103,30 @@ app.post('/webhook', async (req, res) => {
       let responseText = `you said ${params.qty} ${params.pizzaSize} ${params.pizzaFlavours} pizza,your pizza is on the way,this reply came from webhook server`
 
       res.send({
-        "fullfillmentMessages":[
+        "fulfillmentMessages": [
           {
             "text": {
-              "text":[
+              "text": [
                 responseText,
               ]
             }
           }
         ]
       })
+    }else{
+      res.send({
+        "fulfillmentMessages": [
+          {
+            "text": {
+              "text": [
+                "Sorry webhook dont know the answer of your question"
+              ]
+            }
+          }
+        ]
+      })
     }
-  } catch (e) {
+  }catch (e) {
     res.status(500).send({
       message: "server error"
     })
@@ -96,6 +140,10 @@ app.post('/webhook', async (req, res) => {
 // app.use('/', express.static(path.join(_dirname, "web")));
 // // app.use('*' , express.static(path.join(_dirname, "web")));
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on PORT ${PORT}`)
+})
+
 let productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   price: Number,
@@ -105,9 +153,6 @@ let productSchema = new mongoose.Schema({
 });
 const productModel = mongoose.model('products', productSchema);
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 let mongodbUri = 'mongodb+srv://dbuser:snadeema@cluster0.intzdzn.mongodb.net/?retryWrites=true&w=majority'
 
